c33163956c49c8811a8dfab40bba9ff6232a300b
Jerry Chen <jerryq.chen@gmail.com>
2022-07-21 17:58:13 +0800

    added interface

--- dist/app.js
+++ dist/app.js
@@ -1,116 +1,26 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-//abstract class, no instance instantiate
-var Department = /** @class */ (function () {
-    function Department(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
+var plus;
+plus = function (a, b) {
+    return a + b;
+};
+var Person = /** @class */ (function () {
+    //optional parameter
+    function Person(n) {
+        this.name = n;
     }
-    Department.createEmployee = function (name) {
-        return { name: name };
-    };
-    Department.prototype.addEmployee = function (employee) {
-        this.employees.push(employee);
-    };
-    Department.prototype.describeEmployee = function () {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    };
-    //static property
-    Department.fiscalYear = 2022;
-    return Department;
-}());
-var ITDepartment = /** @class */ (function (_super) {
-    __extends(ITDepartment, _super);
-    function ITDepartment(id, admins) {
-        var _this = _super.call(this, id, "IT") || this;
-        _this.admins = admins;
-        return _this;
-    }
-    //having implementation details in child class, based on the abstract method
-    ITDepartment.prototype.describe = function () {
-        console.log("Department ID: " + this.id);
-    };
-    return ITDepartment;
-}(Department));
-var employee1 = Department.createEmployee("Max Manu");
-console.log(employee1, Department.fiscalYear);
-var it = new ITDepartment("d1", ["helpDesk", "Data"]);
-it.describe();
-it.addEmployee("Jerry");
-it.addEmployee("tom");
-it.describeEmployee();
-console.log(it);
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    //private constructor for singleton, instance cannot access it, only class can access
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, "Accounting") || this;
-        _this.reports = reports;
-        _this.lastReport = reports[0];
-        return _this;
-    }
-    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
-        get: function () {
-            if (this.lastReport) {
-                return this.lastReport;
-            }
-            throw new Error("NO REPORTS FOUND");
-        },
-        set: function (value) {
-            if (!value) {
-                return;
-            }
-            this.addReport(value);
-        },
-        enumerable: false,
-        configurable: true
-    });
-    AccountingDepartment.prototype.describe = function () {
-        console.log("Department ID: " + this.id);
-    };
-    //static allows us to access class based method and properties
-    AccountingDepartment.getInstance = function () {
-        if (this.instance) {
-            return this.instance;
+    Person.prototype.greet = function (phrase) {
+        //check if name existed, becos we have made it optional
+        if (this.name) {
+            console.log(phrase + this.name);
         }
-        this.instance = new AccountingDepartment("D2", []);
-        return this.instance;
+        else {
+            console.log("hi");
+        }
     };
-    AccountingDepartment.prototype.addEmployee = function (name) {
-        this.employees.push(name);
-    };
-    AccountingDepartment.prototype.addReport = function (reportName) {
-        this.reports.push(reportName);
-        this.lastReport = reportName;
-    };
-    AccountingDepartment.prototype.printReports = function () {
-        console.log(this.reports);
-    };
-    return AccountingDepartment;
-}(Department));
-var accounting = AccountingDepartment.getInstance();
-console.log(accounting);
-//calling setter function without ()
-accounting.mostRecentReport = "salary wrong...";
-accounting.addReport("bonus wrong...");
-//calling getter function without ()
-console.log(accounting.mostRecentReport);
-accounting.printReports();
-accounting.addEmployee("Manu");
-accounting.describeEmployee();
+    return Person;
+}());
+var person1;
+//name is readOnly restricted by interface
+// person1.name = "Manu";
+person1 = new Person();
+person1.greet("Hello i am ");
--- src/app.ts
+++ src/app.ts
@@ -1,115 +1,47 @@
-//abstract class, no instance instantiate
-abstract class Department {
-    //static property
-    static fiscalYear = 2022;
 
-    protected employees: string[] = [];
-    constructor(protected readonly id: string, public name: string) {
+interface PlusFn {
+    (a: number, b: number): number
+}
+let plus: PlusFn;
+plus = (a: number, b: number) => {
+    return a + b;
+}
+
+//properties in interface and class can be optional
+//function parameter also can be optional using ?
+interface Named {
+    //option interface properties
+    readonly name?: string;
+    outputName?: string;
+}
+interface Greetable extends Named {
+    greet(phrase: string): void;
+}
+
+
+class Person implements Greetable {
+    //optional class properties
+    name?: string;
+    //optional parameter
+    constructor(n?: string) {
+        this.name = n;
     }
+    greet(phrase: string): void {
+        //check if name existed, becos we have made it optional
+        if (this.name) {
+            console.log(phrase + this.name)
+        } else {
+            console.log("hi")
+        }
 
-    static createEmployee(name: string) {
-        return { name: name }
-    }
-
-    //abstract method, no implemention
-    abstract describe(this: Department): void;
-
-    addEmployee(employee: string) {
-        this.employees.push(employee)
-    }
-
-    describeEmployee() {
-        console.log(this.employees.length);
-        console.log(this.employees)
     }
 }
 
-class ITDepartment extends Department {
-    admins: string[];
-    constructor(id: string, admins: string[]) {
-        super(id, "IT");
-        this.admins = admins;
-    }
 
-    //having implementation details in child class, based on the abstract method
-    describe() {
-        console.log("Department ID: " + this.id)
-    }
-}
+let person1: Greetable;
+//name is readOnly restricted by interface
+// person1.name = "Manu";
 
-const employee1 = Department.createEmployee("Max Manu");
-console.log(employee1, Department.fiscalYear)
+person1 = new Person();
 
-const it = new ITDepartment("d1", ["helpDesk", "Data"])
-it.describe();
-it.addEmployee("Jerry")
-it.addEmployee("tom")
-
-it.describeEmployee();
-
-console.log(it)
-
-class AccountingDepartment extends Department {
-    private lastReport: string;
-    //private (Only within class), static (access only through class)
-    private static instance: AccountingDepartment;
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
-        }
-        throw new Error("NO REPORTS FOUND")
-    }
-
-    describe() {
-        console.log("Department ID: " + this.id);
-    }
-
-    set mostRecentReport(value: string) {
-        if (!value) {
-            return
-        }
-        this.addReport(value);
-    }
-    //private constructor for singleton, instance cannot access it, only class can access
-    private constructor(id: string, private reports: string[]) {
-        super(id, "Accounting");
-        this.lastReport = reports[0];
-    }
-
-    //static allows us to access class based method and properties
-    static getInstance() {
-        if (this.instance) {
-            return this.instance
-        }
-        this.instance = new AccountingDepartment("D2", [])
-        return this.instance;
-    }
-
-
-
-    addEmployee(name: string) {
-        this.employees.push(name)
-    }
-
-    addReport(reportName: string) {
-        this.reports.push(reportName)
-        this.lastReport = reportName;
-    }
-
-    printReports() {
-        console.log(this.reports)
-    }
-}
-
-const accounting = AccountingDepartment.getInstance();
-console.log(accounting)
-//calling setter function without ()
-accounting.mostRecentReport = "salary wrong...";
-accounting.addReport("bonus wrong...")
-//calling getter function without ()
-console.log(accounting.mostRecentReport)
-accounting.printReports();
-accounting.addEmployee("Manu");
-accounting.describeEmployee();
-
+person1.greet("Hello i am ");
